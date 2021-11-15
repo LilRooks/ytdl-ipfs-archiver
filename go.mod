f2f14a6166e5b32f16ad72779636c96f8dd7dc0b
Anna Rooks <git.ann@rooks.dev>
2021-11-15 10:55:11 -0500

    now parsing configuration file for binary setting, absolute paths aonly

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/LilRooks/ytdl-ipfs-archiver
 
 go 1.16
+
+require olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
--- internal/pkg/config/parser.go
+++ internal/pkg/config/parser.go
@@ -1,10 +1,25 @@
 package config
 
+import (
+	"io/ioutil"
+
+	"olympos.io/encoding/edn"
+)
+
 type ConfigSet struct {
 	Binary string
 }
 
 // Parse parses the configuration given at path.
 func Parse(path string) (error, ConfigSet) {
-	return nil, ConfigSet{Binary: path}
+	if len(path) == 0 {
+		return nil, ConfigSet{}
+	}
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err, ConfigSet{}
+	}
+	var ret ConfigSet
+	err = edn.Unmarshal(data, &ret)
+	return nil, ret
 }
