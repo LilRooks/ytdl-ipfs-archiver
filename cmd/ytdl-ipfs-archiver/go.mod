c91b4d7cd9c8501ffe71d6db8f99296cadd62331
Anna Rooks <git.ann@rooks.dev>
2021-11-14 20:56:19 -0500

    more restructuring

--- cmd/ytdl-ipfs-archiver/go.mod
+++
@@ -1,5 +0,0 @@
-module main
-
-go 1.16
-
-require github.com/LilRooks/ytdl-ipfs-archiver v0.0.0-20211115002352-66c5bcc797b4
--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/LilRooks/ytdl-ipfs-archiver/internal/cmd"
+	"github.com/LilRooks/ytdl-ipfs-archiver/internal/app"
 )
 
 func main() {
-	if err := cmd.Run(os.Args, os.Stdout); err != nil {
+	if err := app.Run(os.Args, os.Stdout); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err.Error)
 		return err.Code
 	}
